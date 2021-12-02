f8579666f3025d2665525c0d820bb553f5113efa
Robin <robinlmp@mac.com>
2021-12-02 07:44:07 +0000

    Initial Commit

---
+++ AOC_Day2/AOC_Day2App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC_Day2App.swift
+//  AOC_Day2
+//
+//  Created by Robin Phillips on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AOC_Day2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC_Day2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AOC_Day2
+//
+//  Created by Robin Phillips on 02/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
