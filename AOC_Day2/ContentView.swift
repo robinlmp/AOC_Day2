50149084c2e9bb4570595bbf77313845a5712a54
Robin <robinlmp@mac.com>
2021-12-02 08:58:33 +0000

    Initial file loading working. Going to split it up a bit

--- AOC_Day2/ContentView.swift
+++ AOC_Day2/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Text")
+            .onAppear {
+                print("\(loadFile(fileName: "input-2") ?? [""])")
+            }
+    }
+    
+    func loadFile(fileName: String) -> [String]? {
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: "txt"),
+              let contents = try? String(contentsOfFile: filepath) else { return nil }
+        
+        let returnArray = contents.components(separatedBy: "\n").filter { $0 != "" }
+        
+        print(returnArray.count)
+        return returnArray
     }
 }
 
